ef5da5d0d3885d95a1920c13494c45c5df680dbb
Andrew Argeros <aargeros15@gmail.com>
2022-03-08 10:37:12 -0600

    Redefine Image Scrape

--- GetData/GetImages.py
+++ GetData/GetImages.py
@@ -2,17 +2,29 @@ import requests
 import re
 import shutil
 import pandas as pd
-from tqdm import tqdm
-from bs4 import BeautifulSoup
 
-headers = {
-    'User-Agent': 'My User Agent 1.69454',
-    'From': 'youremail@domai55n.com'
-}
+from tqdm import tqdm
+from uuid import uuid4
+from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+def make_headers():
+  headers = {
+    'User-Agent': uuid4().hex,
+    'From': f"{uuid4().hex}@{uuid4().hex}.com"
+  }
+  return headers
 
 def get_images(link, headers, outpath):
   try:
-    response = requests.get(link, headers=headers)
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    response = session.get(link, headers=headers)
     beer_id = re.findall('/\d+/', link)[0].replace('/', '')
 
     soup = BeautifulSoup(response.content)
@@ -28,7 +40,7 @@ def get_images(link, headers, outpath):
       img_link = img.get('data-original')
       if img_link is not None:
         print(img_link)
-        img_file = requests.get(img_link).content
+        img_file = session.get(img_link).content
         with open(f"{outpath}/{beer_id}_{i}.jpg", "wb+") as f:
           f.write(img_file)
         i += 1
@@ -39,7 +51,28 @@ def get_images(link, headers, outpath):
     print("Error")
 
 
-res2 = requests.get('https://untappd.com/beer/top_rated', headers=headers)
+def get_beer_urls(base_link, headers):
+  session = requests.Session()
+  retry = Retry(connect=3, backoff_factor=0.5)
+  adapter = HTTPAdapter(max_retries=retry)
+  session.mount('http://', adapter)
+  session.mount('https://', adapter)
+
+  lsoup = BeautifulSoup(session.get(base_link, headers=headers).content)
+
+  beers = lsoup.find_all('div', {'class': 'beer-details'})
+
+  beer_urls = []
+  for beer in beers:
+    links = beer.find_all('a', href=True)
+    style_link = [link.get('href')
+                  for link in links if link.get('href').startswith('/b/')]
+
+    beer_urls.append(style_link[0])
+  
+  return beer_urls
+
+res2 = requests.get('https://untappd.com/beer/top_rated', headers=make_headers())
 soup2 = BeautifulSoup(res2.content)
 opts = soup2.find_all('select', {'aria-label':'Style picker'})[0].find_all('option')
 
@@ -48,25 +81,20 @@ beer_styles = [opt.get('data-value-slug')
 
 base_link = "https://untappd.com/beer/top_rated?type="
 
-beer_urls = []
+all_beer_urls = []
 
-for slug in beer_styles:
+for slug in tqdm(beer_styles):
   base_link = f"https://untappd.com/beer/top_rated?type={slug}"
-  lsoup = BeautifulSoup(requests.get(base_link, headers=headers).content)
+  all_beer_urls.append(get_beer_urls(base_link, make_headers()))
 
-  beers = lsoup.find_all('div', {'class': 'beer-details'})
+all_beer_urls = [item for sublist in all_beer_urls for item in sublist]
 
-  for beer in beers:
-    links = beer.find_all('a', href=True)
-    style_link = [link.get('href')
-                  for link in links if link.get('href').startswith('/b/')]
-
-    beer_urls.append(style_link[0])
+print(f"Found {len(all_beer_urls)} beers")
 
 all_data = []
-for url in tqdm(beer_urls):
+for url in tqdm(all_beer_urls):
   page_link = 'https://untappd.com' + url + "/photos"
-  data = get_images(page_link, headers, '/content/Images')
+  data = get_images(page_link, make_headers(), './Images')
   all_data.append(data)
   print(data)
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# CDS-5950-Capstone
+# CDS 5950- Computer Vision Capstone Project
+
+
+## Data 
+
+The data for this project come from [Untapped](https://untapped.com/) Top Beers by Style. Images are then parsed into pictures of beers vs pictures of cans, and resized for use in training. 
  
